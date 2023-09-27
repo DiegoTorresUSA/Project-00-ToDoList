9d9ef29d95adaaf8f65fa3166ed652784389524c
Valen <valen.araya@gmail.com>
2023-09-27 18:38:15 -0300

    Rama Valen. Agrega tareas en js

---
+++ js/app.js
@@ -0,0 +1,36 @@
+//LISTA DE TAREAS
+let listaTareas = [];
+let id = 1;
+const form = document.querySelector("#formulario");
+const taskInput = document.querySelector("#input-task")
+
+//EVENTOS
+form.addEventListener("submit", addTask);
+
+ 
+//AGREGAR TAREAS
+function addTask(event){
+    event.preventDefault();
+    if(taskInput.value.trim() !== ""){
+        const task = {
+            id: id,
+            status: "toDo",
+            value: taskInput.value,
+        };
+
+        listaTareas.push(task);
+        id++;
+        form.reset();
+        // console.log(listaTareas);
+    }else{
+        alert("Debe rellenar el input");
+    }
+}
+
+
+//ELIMINAR TAREAS
+
+//ALMACENAR EN LOCALSTORAGE
+
+
+//CARGAR EN LOCALSTORAGE
